aa30ee5bda9bb49e888708e624a227f8d175023c
Graybeggar <blhat@hotmail.com>
2025-03-15 19:54:11 -0600

    Latest-Update variables-naming-conventions-and-top-to-bottom-code-evaluation.js

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,11 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let customerName = "Alice";
+let numItems = 5;
+let totalCost = 20;
+let place = "The Pub";
+let totalPrice = numItems * totalCost;
+let purchaseMessage = customerName + " bought " + numItems + " items for $" + totalCost + " at " + place;
 
-console.log(d);
+console.log(purchaseMessage);
